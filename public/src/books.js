178a601fce944e66e0490b9b761152d69e3c0302
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-02-27 12:23:26 -0600

    findBookById() passes tests

--- public/src/books.js
+++ public/src/books.js
@@ -5,7 +5,10 @@ function findAuthorById(authors, id) {
   return authors.find(author => author.id === id);
 }
 
-function findBookById(books, id) {}
+// *returns the book object that has the matching ID
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
 
 function partitionBooksByBorrowedStatus(books) {}
 
