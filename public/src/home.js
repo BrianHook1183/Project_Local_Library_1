1727b1748ef7bb04a1ea8a2e60b10726fe09472c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-02-27 14:57:13 -0600

    getTotalBooksCount() passes tests

--- public/src/home.js
+++ public/src/home.js
@@ -1,6 +1,9 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
-function getTotalBooksCount(books) {}
+// *returns a number that represents the number of book objects inside of the array.
+function getTotalBooksCount(books) {
+  return books.length;
+}
 
 function getTotalAccountsCount(accounts) {}
 
