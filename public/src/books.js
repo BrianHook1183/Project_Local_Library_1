f23433672001ed48fa029d27f702f48fd7ab9aaa
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-02-27 14:53:40 -0600

    getBorrowersForBook() passing tests

--- public/src/books.js
+++ public/src/books.js
@@ -25,7 +25,48 @@ function partitionBooksByBorrowedStatus(books) {
   return [unavailableBooks, availableBooks];
 }
 
-function getBorrowersForBook(book, accounts) {}
+// *returns an array of all the transactions from the book's borrows key. However, each transaction should include the related account information and the returned key.
+function getBorrowersForBook(book, accounts) {
+  const borrowLog = book.borrows.reduce((acc, transaction) => {
+    // finds the account object with matching id
+    let matchedAccount =  accounts.find(account => account.id === transaction.id);
+    // spreads objects together, id key is shared between both so it's overwritten but nothing is affected
+    acc.push({...transaction, ...matchedAccount});
+    return acc;
+  }, []);
+  // Limit to 10 results
+  return borrowLog.slice(0, 10);
+}
+/* Example Output:  getBorrowersForBook(book, accounts);
+  [
+    {
+      id: "5f446f2e4eff1030e7316861",
+      returned: true,
+      picture: "https://api.adorable.io/avatars/75/barber.waters@kegular.biz",
+      age: 37,
+      name: {
+        first: "Barber",
+        last: "Waters",
+      },
+      company: "KEGULAR",
+      email: "barber.waters@kegular.biz",
+      registered: "Tuesday, April 14, 2020 9:15 PM",
+    },
+    {
+      id: "5f446f2ecc5c4787c403f844",
+      returned: true,
+      picture: "https://api.adorable.io/avatars/75/dyer.trevino@slax.io",
+      age: 34,
+      name: {
+        first: "Dyer",
+        last: "Trevino",
+      },
+      company: "SLAX",
+      email: "dyer.trevino@slax.io",
+      registered: "Saturday, August 1, 2015 8:13 PM",
+    },
+  ]
+*/
 
 module.exports = {
   findAuthorById,
