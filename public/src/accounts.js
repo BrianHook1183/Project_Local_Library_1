1dd3312706d40dd9f8762d7c55fde12daddc8aed
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-02-27 02:14:51 -0600

    getTotalNumberOfBorrows test is passing

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -10,7 +10,16 @@ function sortAccountsByLastName(accounts) {
   return accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+// *returns a number that represents the number of times the account's ID appears in any book's borrow array.
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    // forEach allows checking for multiple borrows of the same book
+   book.borrows.forEach(borrow => {
+     if (borrow.id === account.id) acc += 1;
+    });
+    return acc;
+  }, 0);
+}
 
 function getBooksPossessedByAccount(account, books, authors) {}
 
